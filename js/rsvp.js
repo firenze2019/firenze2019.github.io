d164c23eac393839ef54c811b9d9fff89de9a928
firenze2019 <tbmbknupfer@gmail.com>
2018-01-10 16:13:07 -0500

    fixed rsvp so no redirect to formspree

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,25 +1,42 @@
 $( document ).ready(function() {
 
     $('form').submit(function(event) {
-        event.preventDefault();
+      event.preventDefault();
 
-        $.ajax({
-            url: "http://formspree.io/tbmbknupfer@gmail.com",
-            method: "POST",
-            data: {
-              name: $('name').val()
-              email: $('_replyto').val(),
-              maybeRSVP: $('rsvpDecisionMaybe').val()
-              noRSVP: $('rsvpDecisionNo').val()
-              address: $('address').val()
-            },
-            dataType: "json"
-        }).error(function(jqXHR, status, error){
-            alert('Uh oh, something went wrong. Please try again.');
-        }). success(function(data, status, jqXHR) {
-            alert('Thank you for your early bird RSVP!');
-            $('form').find('name, _replyto, rsvpDecisionMaybe, rsvpDecisionNo, address').val('');
-        });
+      $.ajax({
+          url: "https://formspree.io/tbmbknupfer@gmail.com",
+          method: "POST",
+          data: {
+            email: $('#js-email').val(),
+            address: $('#js-address').val()
+          },
+          dataType: "json"
+
+      }).error(function(jqXHR, status, error){
+        alert("Uh oh, something went wrong and your rsvp was not sent");
+      }).success(function(data, status, jqXHR) {
+          alert("Thank you for sending in your early bird rsvp!")
+          /*Empty form values so it's obvious to user rsvp was sent*/
+          $('form').find('#js-email, #js-address').val('');
+      });
+   });
+/*
+    document.getElementById('js-maybeRSVP').onchange = function() {
+      document.getElementById('js-address').disabled = !this.checked;
+    };
+
+     */
+/*
+    $("input[name='rsvpDecisionNo']").click(function(){
+               if ($(this).is(':checked')) {
+                      document.getElementById('js-address').attr("disabled", false);
+                 }
+               else if ($(this).not(':checked')) {
+                    var remove = '';
+                      document.getElementById('js-address').attr('value', remove);
+                      document.getElementById('js-address').attr("disabled", true);
+                }
     });
+    */
 
 });
--- js/website.js
+++ js/website.js
@@ -7,6 +7,9 @@
      });
 
      gallery.on('jg.complete', function() {
-        $('.swipeboxImg').swipebox()
+        $('.swipeboxImg').swipebox({
+          hideBarsDelay : 3000,
+          loopAtEnd: true,
+        })
     });
  });
