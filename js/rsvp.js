c3817e812c30f1087637b8bb0e6e71d9f2c8e425
firenze2019 <tbmbknupfer@gmail.com>
2018-01-10 11:14:20 -0500

    rsvp.js ids changed to names

--- js/rsvp.js
+++ js/rsvp.js
@@ -7,18 +7,18 @@ $( document ).ready(function() {
             url: "http://formspree.io/tbmbknupfer@gmail.com",
             method: "POST",
             data: {
-              name: $('#js-name').val()
-              email: $('#js-email').val(),
-              maybeRSVP: $('#js-maybeRSVP').val()
-              noRSVP: $('#js-noRSVP').val()
-              address: $('#js-address').val()
+              name: $('name').val()
+              email: $('_replyto').val(),
+              maybeRSVP: $('rsvpDecisionMaybe').val()
+              noRSVP: $('rsvpDecisionNo').val()
+              address: $('address').val()
             },
             dataType: "json"
         }).error(function(jqXHR, status, error){
             alert('Uh oh, something went wrong. Please try again.');
         }). success(function(data, status, jqXHR) {
             alert('Thank you for your early bird RSVP!');
-            $('form').find('#js-name, #js-email, #js-address, #js-maybeRSVP, #js-noRSVP').val('');
+            $('form').find('name, _replyto, rsvpDecisionMaybe, rsvpDecisionNo, address').val('');
         });
     });
 
