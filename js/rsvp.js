83d9e64980c13fabfec06c7a5578d71f0b750930
firenze2019 <tbmbknupfer@gmail.com>
2018-01-10 11:09:39 -0500

    checkbox deselects

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,25 +1,25 @@
 $( document ).ready(function() {
 
-      $('form').submit(function(event) {
-          event.preventDefault();
+    $('form').submit(function(event) {
+        event.preventDefault();
 
-          $.ajax({
-              url: "http://formspree.io/tbmbknupfer@gmail.com",
-              method: "POST",
-              data: {
-                  name: $('#js-name').val()
-                  email: $('#js-email').val(),
-                  address: $('#js-address').val()
-                  maybeRSVP: $('#js-maybeRSVP').val()
-                  noRSVP: $('#js-noRSVP').val()
-              },
-              dataType: "json"
-          }).error(function(jqXHR, status, error){
-              alert('Uh oh, something went wrong. Please try again.');
-          }). success(function(data, status, jqXHR) {
-              alert('Thank you for your RSVP');
-              $('form').find('#js-email, #js-address, #js-rsvp').val('');
-          });
-      });
+        $.ajax({
+            url: "http://formspree.io/tbmbknupfer@gmail.com",
+            method: "POST",
+            data: {
+              name: $('#js-name').val()
+              email: $('#js-email').val(),
+              maybeRSVP: $('#js-maybeRSVP').val()
+              noRSVP: $('#js-noRSVP').val()
+              address: $('#js-address').val()
+            },
+            dataType: "json"
+        }).error(function(jqXHR, status, error){
+            alert('Uh oh, something went wrong. Please try again.');
+        }). success(function(data, status, jqXHR) {
+            alert('Thank you for your early bird RSVP!');
+            $('form').find('#js-name, #js-email, #js-address, #js-maybeRSVP, #js-noRSVP').val('');
+        });
+    });
 
-  });
+});
