479cb48e790cc0f5a64fe0b2274f4de439c518c4
firenze2019 <tbmbknupfer@gmail.com>
2018-01-23 13:34:47 -0500

    only have _subject in html not js

--- js/rsvp.js
+++ js/rsvp.js
@@ -11,8 +11,7 @@ $( document ).ready(function() {
             email: $('#js-email').val(),
             rsvpDecisionMaybe: $('#js-maybeRSVP:checked').val(),
             rsvpDecisionNo: $('#js-noRSVP:checked').val(),
-            address: $('#js-address').val(),
-            _subject: "RSVP from TBM Wedding"
+            address: $('#js-address').val()
           },
           dataType: "json"
 
