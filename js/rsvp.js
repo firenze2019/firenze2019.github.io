4a9956a2456b0ab5cf021970846093947f35cf88
firenze2019 <tbmbknupfer@gmail.com>
2018-01-25 21:08:47 -0500

    trying to go back working still with radio

--- js/rsvp.js
+++ js/rsvp.js
@@ -9,8 +9,8 @@ $( document ).ready(function() {
           data: {
             name: $('#js-name').val(),
             email: $('#js-email').val(),
-            rsvpDecision: $('#js-maybeRSVP:checked').val(),
-            rsvpDecision: $('#js-noRSVP:checked').val(),
+            rsvpDecisionMaybe: $('#js-maybeRSVP:checked').val(),
+            rsvpDecisionNo: $('#js-noRSVP:checked').val(),
             address: $('#js-address').val()
           },
           dataType: "json"
